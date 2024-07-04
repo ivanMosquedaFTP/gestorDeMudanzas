ecc52e846067eae7feb1f2feb2f4a593502cb21c
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-07-04 10:40:23 -0600

    rebase

--- src/main/java/com/example/testproject/HelloApplication.java
+++ src/main/java/com/example/testproject/HelloApplication.java
@@ -7,21 +7,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(getClass().getResource("/css/main.css").toString());
-        stage.setTitle("Moving services.");
-        stage.setScene(scene);
-        HelloController helloController = fxmlLoader.getController();
-        helloController.setStage(stage);
-        stage.show();
-        stage.setMaximized(true);
-    }
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    Scene scene = new Scene(fxmlLoader.load());
+    scene.getStylesheets().add(getClass().getResource("/css/main.css").toString());
+    stage.setTitle("Moving services.");
+    stage.setScene(scene);
+    HelloController helloController = fxmlLoader.getController();
+    helloController.setStage(stage);
+    stage.show();
+    stage.setMaximized(true);
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 }
